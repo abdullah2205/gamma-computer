9a4ab501e3b2008eda8137ba2f26884588b160d1
Muhammad Abdullah <ma22052000@gmail.com>
2020-11-15 05:58:26 +0700

    pemanis buatan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron mb-3 jumbotron-fluid">
       <div class="container">
-        <h1 class="display-4">Admin Dashboard</h1>
+        <h1 class="display">Admin Dashboard</h1>
         <p class="lead">Selamat Datang di Admin Dashboard.</p>
       </div>
     </div>
